7a3aa8073943cc03f10401dbd40932f00647458a
beratridvanasilturk <br.asltrk@gmail.com>
2024-01-06 22:30:06 +0300

    Added React-Native Navigator Package

---
+++ TaskList/App.tsx
@@ -0,0 +1,37 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  useColorScheme,
+} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import MainView from './screens/MainView';
+import AddTaskView from './screens/AddTaskView';
+
+function App(): React.JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const Stack = createNativeStackNavigator();
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name = "Main" component={MainView} />
+        <Stack.Screen name = "AddTask" component={AddTaskView} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+});
+
+export default App;
---
+++ TaskList/screens/MainView.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function MainView() {
+  return (
+    <View>
+      <Text>MainView</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
