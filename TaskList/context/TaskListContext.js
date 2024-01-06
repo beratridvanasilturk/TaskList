8dd6df81f14c41fc841c3e738bcec880c2eaf832
beratridvanasilturk <br.asltrk@gmail.com>
2024-01-07 02:03:59 +0300

    Added Data Transfer Array with useContext

--- TaskList/context/TaskListContext.js
+++ TaskList/context/TaskListContext.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState } from 'react';
 
 // TaskListContext'i data transferi yapmak icin kullandik.
 const TaskListContext = React.createContext();
@@ -6,9 +7,15 @@ const TaskListContext = React.createContext();
 // Data alisverisi icin kullanilacak olan provider'dir. App dosyasinda sayfalarin arasinda veri alisverisi icin kullanilir.
 // Main component olan App.js dosyasinda kullanilir. Child componentler de buradan veri alisverisi yapabilirler.
 export const TaskListProvider = ({ children }) => {
-    const sampleArray = [{title: 'ReactNative'}, {title: 'SwiftUI'}];
+    const [sampleArray, setSampleArray] = useState([
+        {title: 'ReactNative'},
+        {title: 'SwiftUI'}
+    ]);
+    const addNewTask = () => {
+        setSampleArray([...sampleArray, {title: 'newTask'}]);
+    };
     return (
-        <TaskListContext.Provider value={sampleArray}>
+        <TaskListContext.Provider value={{data: sampleArray, addNewTask}}>
         {children}
         </TaskListContext.Provider>
     );
--- TaskList/screens/MainView.js
+++ TaskList/screens/MainView.js
@@ -1,15 +1,17 @@
-import { Text, View, FlatList } from 'react-native'
+import { Text, View, FlatList, Button } from 'react-native'
 import React, { useContext } from 'react'
 import TaskListContext from '../context/TaskListContext'
 
 export default function MainView() {
+  const { data, addNewTask } = useContext(TaskListContext);
   // Cekmek istedigimiz context'i burada useContext ile cekiyoruz.
-  const sampleArray = useContext(TaskListContext);
+  // const sampleArray = useContext(TaskListContext);
   return (
     <View>
       <Text>MainView</Text>
+      <Button title="Add New Task" onPress={addNewTask}/>
       <FlatList
-        data={sampleArray}
+        data={data}
         keyExtractor={propInArray => propInArray.title}
         /* renderItem icerisindeki item SwiftUI'daki forEach icerisindeki her bir dongu elemanini ifade eder */
         renderItem={({ item }) => {
